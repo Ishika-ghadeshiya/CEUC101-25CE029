05b77f17621b60f6a5ff65fc3f22d752c8677709
Ishika-ghadeshiya <ghadeshiyaishika@gmail.com>
2025-07-18 15:57:37 +0530

    Add files via upload

---
+++ Practical 2/practical 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    // This program contains area and perimeter of a park
+    int Length = 70;
+    int Breadth =90;
+    printf(" The area of park is: %d", Length*Breadth);
+    printf("\n The perimeter of park is: %d",2*(Length+Breadth));
+    printf("\n This program is developed by Ishika_25TCEVBN");
+    return 0;
+
+}
