c51d3ddc082713705d069dfad941c387db6ace99
Ishika-ghadeshiya <ghadeshiyaishika@gmail.com>
2025-08-18 15:45:03 +0530

    Add files via upload

---
+++ Practical 13/Practical 13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int water_level=0;
+    printf("It is an automatic tank refill system");
+    for(water_level=0;water_level<=100;water_level=water_level+10)
+    {
+        printf("\nCurrent water level:%d",water_level);
+
+    }
+
+       printf("\nTank is Full");
+       printf("\nThis program is developed by Ishika_25TCEVBN");
+}
