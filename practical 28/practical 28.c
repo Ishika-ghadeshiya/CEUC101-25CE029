8aec5d0124d6dcc6637fe073c222a808ed7c5f96
Ishika-ghadeshiya <ghadeshiyaishika@gmail.com>
2025-11-18 22:46:50 +0530

    Add files via upload

---
+++ practical 28/practical 28.c
@@ -0,0 +1,38 @@
+
+#include<stdio.h>
+#include<string.h>
+
+union book_details
+{
+    int bid;
+    char book_name[50],auther[50], status;
+    float price;
+};
+void main()
+{
+     int t;
+     char  Tbook_name[50],Tauther[50],Tstatus;
+     float Tprice;
+   union book_details s;
+   printf("Enter book id\n");
+   scanf("%d",&s.bid);
+    t=s.bid;
+    printf("Enter title of book\n");
+   scanf(" %[^\n]",s.book_name);
+   strcpy(Tbook_name,s.book_name);
+    printf("Enter auther\n");
+   scanf(" %[^\n]",s.auther);
+   strcpy(Tauther,s.auther);
+    printf("Enter status\n");
+   scanf(" %c",&s.status);
+   strcpy(Tstatus,s.status);
+    printf("Enter price\n");
+   scanf(" %f",&s.price);
+    Tprice=s.price;
+
+   printf("\n book id: %d\n",t);
+   printf("\ntitle of book: %s",Tbook_name);
+   printf("\nauther :%s",Tauther);
+   printf("\nstatus:%c",Tstatus);
+   printf("\nprice:%f", Tprice);
+}
